08efeb5b8c667727c658c39a9ff5cf823a59eca5
Felipe G. Brandão <feeeh@gf2.com.br>
2022-12-21 11:04:44 -0300

    Mais melhoras nos comentários

--- dist/js/custom_flatpickr.js
+++ dist/js/custom_flatpickr.js
@@ -1,7 +1,7 @@
 // DIV COM TODOS OS EVENTOS
 var eventoSesiDiv = $("#eventosSesi");
 
-// STRING DE DATAS, ONDE GRAVA TODAS AS DATAS PARA COLOCAR NO CALENDÁRIO
+// ARRAY STRING DE DATAS, ONDE GRAVA TODAS AS DATAS PARA COLOCAR NO CALENDÁRIO
 var datas = [];
 
 // PEGA O ÚLTIMO DIA DO PRÓXIMO MÊS - PARA A ÚLTIMA DATA DO CALENDÁRIO
@@ -17,31 +17,33 @@ function addData(data, titulo, subtitulo) {
 
     // DATAS
     // TODAS AS DATAS PRECISAM SER DO TIPO "YYYY-MM-DD"
-    /// SE TEM ":", GERALMENTE O QUE VEM DA API TEM :
-    /// TRANSFORMA EX. "20/12/2022 13:44:27" PARA "2022-12-20"
+    /// Se tem ":" (Ex. "20/12/2022 13:44:27")
+    /// Transforma o exemplo de cima para o formato certo
     if (data.indexOf(":") !== -1) {
         var dataArr = data.split(" ");
         var dataArr2 = dataArr[0].split("/");
         data = dataArr2[2] + "-" + dataArr2[1] + "-" + dataArr2[0];
         datas.push(data);
     }
-    /// SE TEM "-"
-    /// TRANSFORMA EX. "20-12-2022" PARA "2022-12-20", CASO NECESSÁRIO
+    
+    /// Se tem "-" (Ex. "2022-12-20", ou "20-12-2022")
+    /// Caso primeiro exemplo, só adiciona na lista
+    /// Se for o segundo exemplo, arruma a data para o formato certo
     else if (data.indexOf("-") !== -1) {
         var dataSplit = data.split("-");
         if (dataSplit[0].length !== 4)
             data = dataSplit[2] + "-" + dataSplit[1] + "-" + dataSplit[0];
         datas.push(data);
     }
-    /// SE TEM "/"
-    /// TRANSFORMA EX. "20/12/2022" PARA "2022-12-20"
+    /// Se tem "/" (Ex. "20/12/2022")
+    /// Transforma o exemplo de cima para o formato certo
     else if (data.indexOf("/") !== -1) {
         var dataArr = data.split("/");
         data = dataArr[2] + "-" + dataArr[1] + "-" + dataArr[0];
         datas.push(data);
     }
-    /// SE SÓ TEM NÚMERO - É UM UNIX TIMESTAMP
-    /// TRANSFORMA EX. "1671569063" PARA "2022-12-20"
+    /// Se só tem número (Unix Timestamp - Ex. "1671569063")
+    /// Converte o exemplo de cima para o formato certo
     else if (data.match(/^-?\d+$/)) {
         var dataCerta = new Date(parseInt(data, 10) * 1000);
         data = dataCerta.getFullYear() + "-" + ("0" + (dataCerta.getMonth() + 1)).slice(-2) + "-" + ("0" + dataCerta.getDate()).slice(-2);
@@ -76,7 +78,7 @@ function criarCalendario() {
         maxDate: getLastDayOfNextMonth(),
         enable: datas,
 
-        // ONCHANGE É O EVENTO DE QUANDO CLICA EM ALGUMA DATA NO CALENDÁRIO
+        // ONCHANGE É O EVENTO DE QUANDO TROCA DATA NO CALENDÁRIO
         // MOSTRA A DIV "MÃE" E ESCONDE TODOS AS DIVS COM EVENTOS VISÍVEIS
         // DEPOIS MOSTRA SÓ OS EVENTOS COM A DATA CLICADA
         onChange: function (dateObj, dateStr) {
@@ -87,8 +89,7 @@ function criarCalendario() {
         },
 
         // ONDAYCREATE É UM EVENTO QUE QUANDO ESTÁ CRIANDO AS DATAS (QUE EXISTEM), ELE FAZ ALGO
-        // NESSE CASO, ADICIONA UMA CLASSE EM TODOS AS DATAS QUE TEM ALGO E ADICIONA A CLASSE EVENTO
-        // PARA ADICIONAR A BOLINHA EM BAIXO DO EVENTO COM ALGO
+        // NESSE CASO, ADICIONA UMA CLASSE EM TODOS AS DATAS QUE TEM ALGO QUE COLOCA A BOLINHA EM BAIXO DO EVENTO
         onDayCreate: function (dObj, dStr, fp, dayElem) {
             if (!$(dayElem).hasClass("flatpickr-disabled"))
                 dayElem.innerHTML += "<span class='event'></span>";
